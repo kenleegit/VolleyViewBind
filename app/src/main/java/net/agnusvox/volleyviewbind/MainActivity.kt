ac951acab7455e53c847f1ffcc430dc2fdec4515
kenleegit <ahkenlee@yahoo.com.hk>
2019-10-26 20:50:49 +0800

    Make simple volley request to get a string

--- app/src/main/java/net/agnusvox/volleyviewbind/MainActivity.kt
+++ app/src/main/java/net/agnusvox/volleyviewbind/MainActivity.kt
@@ -5,6 +5,11 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +24,29 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        getMyText()
+    }
+
+    //Reference: https://developer.android.com/training/volley/simple
+    private fun getMyText() {
+        val textView = findViewById<TextView>(R.id.myText)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://ly729.airtime.pro/api/live-info/?callback"
+
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
